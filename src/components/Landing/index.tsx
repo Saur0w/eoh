06717421282f16c275b6153f6f30636a087e93aa
saur0w <sthapliyal085@gmail.com>
2026-05-20 11:44:15 +0530

    feat: create Landing component with associated stylesheet and typography styles

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -5,7 +5,9 @@ import styles from "./style.module.scss";
 export default function Landing() {
     return (
         <section className={styles.landing}>
-            
+            <div className={styles.heading}>
+                <h1>Nature.<br />Ritual.<br />Restoration.</h1>
+            </div>
         </section>
     );
 }
