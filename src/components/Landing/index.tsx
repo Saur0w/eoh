08e697a8eff1141adb5b7a01521379c87cebb72b
saur0w <sthapliyal085@gmail.com>
2026-05-23 21:46:41 +0530

    "Button"

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -2,6 +2,7 @@
 
 import styles from "./style.module.scss";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Landing() {
     return (
@@ -21,6 +22,9 @@ export default function Landing() {
                 <p>A soulful expression of beauty.<br />
                     Thoughtfully created to bring peace, grace, and<br /> meaning into everyday.</p>
             </div>
-            </section>
+            <div className={styles.button}>
+                <Link href="/contact">Book a consultation</Link>
+            </div>
+        </section>
     );
 }
