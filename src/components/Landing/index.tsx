e8487ca1d886189406aa12dbe7f485874dd1438d
saur0w <sthapliyal085@gmail.com>
2026-05-23 18:35:34 +0530

    feat: implement landing page component with styling

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -6,13 +6,21 @@ import Image from "next/image";
 export default function Landing() {
     return (
         <section className={styles.landing}>
+            <div className={styles.tag}>
+                <h5>-WELLNESS ROOTED IN NATURE</h5>
+            </div>
             <div className={styles.heading}>
-                <h1>Cultivating Stillness.<br />
-                    Curating Nature</h1>
+                <h1>Cultivating<br /> stillness.<br />
+                    Curating nature.</h1>
             </div>
             <div className={styles.imageContainer}>
                 <Image src="/images/landing.webp" alt="landing" height={800} width={600} />
             </div>        
+            <div className={styles.line}/>
+            <div className={styles.para}>
+                <p>A soulful expression of beauty.<br />
+                    Thoughtfully created to bring peace, grace, and<br /> meaning into everyday.</p>
+            </div>
             </section>
     );
 }
