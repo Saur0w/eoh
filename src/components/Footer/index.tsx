d938a1dc5bce1ad0cb72bc836203e96bd23e49be
saur0w <sthapliyal085@gmail.com>
2026-05-29 21:44:33 +0530

    feat: enhance Footer with new subheading, logo, and MagneticWrapper integration

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,7 +5,10 @@ import Link from "next/link";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { SplitText } from "gsap/SplitText";
-import { useGSAP } from "@gsap/react"
+import { useGSAP } from "@gsap/react";
+import Image from "next/image";
+import RoundedButton from "@/ui/RoundedButton";
+import MagneticWrapper from "@/ui/Magnetic";
 
 gsap.registerPlugin(ScrollTrigger, SplitText, useGSAP);
 
@@ -14,6 +17,20 @@ export default function Footer() {
         <footer className={styles.footer}>
             <div className={styles.heading}>
                 <h1>Let&#39;s craft an experience together.</h1>
+                <div className={styles.sub}>
+                    <div className={styles.imageContainer}>
+                        <Image src="/images/logo.png" alt="logo" height={70} width={70} />
+                    </div>
+                    <h3>Deepika singh, <br />Founder of Essene of Hope</h3>
+                </div>
+                <div className={styles.lb}>
+                    <div className={styles.line} />
+                    <div className={styles.button}>
+                        <MagneticWrapper>
+                            <p>Connect</p>
+                        </MagneticWrapper>
+                    </div>
+                </div>
             </div>
             <div className={styles.navigation}>
                 <h4>Navigation</h4>
